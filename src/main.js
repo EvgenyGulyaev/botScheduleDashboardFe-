2a30f322f8dc2ddcc869caa121be444745f81704
Евгений Гуляев <wardercompany@gmail.com>
2025-12-11 14:11:03 +0300

    update

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import './style.css'
+import { useAuthStore } from './stores/auth'
 
 const app = createApp(App)
-app.use(createPinia())
+const pinia = createPinia()
+
+app.use(pinia)
 app.use(router)
+
+const authStore = useAuthStore()
+authStore.init()
+
 app.mount('#app')
