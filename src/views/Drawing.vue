38774f472e72b408dfc8085f2ba63801a5c7ceff
evgeny.gulyaev <Evgeny.Gulyaev@x5.ru>
2026-06-04 08:14:25 +0300

    fix: align drawing canvas under toolbar

--- src/views/Drawing.vue
+++ src/views/Drawing.vue
@@ -471,7 +471,7 @@
 
       <div
         ref="editorStageRef"
-        class="flex min-h-0 flex-1 items-center justify-center overflow-hidden"
+        class="flex min-h-0 flex-1 items-start justify-center overflow-hidden"
       >
         <canvas
           ref="canvasRef"
